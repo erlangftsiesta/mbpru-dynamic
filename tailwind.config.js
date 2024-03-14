560cd18b533a1f924f774e10bec4585b0361e5fc
erlangftsiesta12 <erlangftsiesta12@gmail.com>
2024-03-14 22:26:21 +0700

    menambahkan konfigurasi tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["../frontend/**/*.{html,ejs,js}"],
     theme: {
       extend: {},
     },
